e9491e5731fbe8b2bfa141ce70518e7d2d2b2247
Anon <trunk-geta@web.de>
2020-11-22 13:51:37 +0100

    v0.8

--- game_logic.js
+++ game_logic.js
@@ -32,7 +32,7 @@ function startTimer(duration, display) {
 }
 
 function timer() {
-    let Minutes = 60 * 1,
+    let Minutes = 60 * 0.2,
         display = document.getElementById("timer");
     startTimer(Minutes, display);
 };
@@ -44,9 +44,9 @@ let woodBuildingLevel = 0;
 let stoneBuildingLevel = 0;
 let ironBuildingLevel = 0;
 
-let wood = 300;
-let stone = 250;
-let tools = 150;
+let wood = 3000;
+let stone = 2500;
+let tools = 1500;
 
 let resources_array = [wood,stone,tools];
 
@@ -72,8 +72,7 @@ function upgradeWood(){
 			document.getElementById("resource_wood").innerHTML = resources_array[0];
 			document.getElementById("resource_stone").innerHTML =  resources_array[1];
 			resourcesNextLevelWoodRequiredCalc();
-		}
-	 else {
+		} else {
 		console.log("Not enough resources.");
 	}
 }
@@ -102,8 +101,7 @@ function upgradeStone(){
 			document.getElementById("resource_wood").innerHTML =resources_array[0];
 			document.getElementById("resource_stone").innerHTML = resources_array[1];
 			resourcesNextLevelStoneRequiredCalc();
-		}
-	 else {
+		} else {
 		console.log("Not enough resources.");
 	}
 }
@@ -132,8 +130,7 @@ function upgradeIron(){
 			document.getElementById("resource_wood").innerHTML =resources_array[0];
 			document.getElementById("resource_stone").innerHTML = resources_array[1];
 			resourcesNextLevelToolsRequiredCalc();
-		}
-	 else {
+		} else {
 		console.log("Not enough resources.");
 	}
 }
@@ -157,7 +154,8 @@ function resourcesNextLevelWoodRequiredCalc() {
 	resourceNextLevelArray[1] = stone;
 	resourceNextLevelArray[2] = tools;
 
-	document.getElementById("mainCenterInfoNextLevelWoodRequiredResources").innerHTML = "Wood : " + resourceNextLevelArray[0] + " Stone: " + resourceNextLevelArray[1] + " Tools: " + resourceNextLevelArray[2] ;	
+	document.getElementById("mainCenterInfoNextLevelWoodRequiredResources").innerHTML = 
+	"Wood : " + resourceNextLevelArray[0] + " Stone: " + resourceNextLevelArray[1] + " Tools: " + resourceNextLevelArray[2] ;	
 }
 
 function resourcesNextLevelStoneRequiredCalc() {
@@ -169,7 +167,8 @@ function resourcesNextLevelStoneRequiredCalc() {
 	resourceNextLevelArray[1] = stone;
 	resourceNextLevelArray[2] = tools;
 
-	document.getElementById("mainCenterInfoNextLevelStoneRequiredResources").innerHTML = "Wood : " + resourceNextLevelArray[0] + " Stone: " + resourceNextLevelArray[1] + " Tools: " + resourceNextLevelArray[2] ;
+	document.getElementById("mainCenterInfoNextLevelStoneRequiredResources").innerHTML = 
+	"Wood : " + resourceNextLevelArray[0] + " Stone: " + resourceNextLevelArray[1] + " Tools: " + resourceNextLevelArray[2] ;
 	
 }
 
@@ -182,7 +181,8 @@ function resourcesNextLevelToolsRequiredCalc() {
 	resourceNextLevelArray[1] = stone;
 	resourceNextLevelArray[2] = tools;
 
-	document.getElementById("mainCenterInfoNextLevelToolsRequiredResources").innerHTML = "Wood : " + resourceNextLevelArray[0] + " Stone: " + resourceNextLevelArray[1] + " Tools: " + resourceNextLevelArray[2] ;
+	document.getElementById("mainCenterInfoNextLevelToolsRequiredResources").innerHTML = 
+	"Wood : " + resourceNextLevelArray[0] + " Stone: " + resourceNextLevelArray[1] + " Tools: " + resourceNextLevelArray[2] ;
 }
 
 
@@ -368,8 +368,7 @@ function ship1Constructor() {
 			document.getElementById("resource_wood").innerHTML =resources_array[0];
 			document.getElementById("resource_stone").innerHTML = resources_array[1];
 			document.getElementById("resource_iron").innerHTML = resources_array[2];
-		}
-	 else {
+		} else {
 		console.log("Not enough resources.");
 	}
 }
@@ -395,8 +394,7 @@ function ship2Constructor() {
 			document.getElementById("resource_wood").innerHTML =resources_array[0];
 			document.getElementById("resource_stone").innerHTML = resources_array[1];
 			document.getElementById("resource_iron").innerHTML = resources_array[2];
-		}
-	 else {
+		} else {
 		console.log("Not enough resources.");
 	}
 }
@@ -422,8 +420,7 @@ function ship3Constructor() {
 			document.getElementById("resource_wood").innerHTML =resources_array[0];
 			document.getElementById("resource_stone").innerHTML = resources_array[1];
 			document.getElementById("resource_iron").innerHTML = resources_array[2];
-		}
-	 else {
+		} else {
 		console.log("Not enough resources.");
 	}
 }
@@ -450,9 +447,8 @@ function tower1Constructor() {
 			document.getElementById("resource_wood").innerHTML =resources_array[0];
 			document.getElementById("resource_stone").innerHTML = resources_array[1];
 			document.getElementById("resource_iron").innerHTML = resources_array[2];
-		}
-	 else {
-		console.log("Not enough resources.");
+		} else {
+			console.log("Not enough resources.");
 	}
 }
 
@@ -478,8 +474,7 @@ function tower2Constructor() {
 			document.getElementById("resource_wood").innerHTML =resources_array[0];
 			document.getElementById("resource_stone").innerHTML = resources_array[1];
 			document.getElementById("resource_iron").innerHTML = resources_array[2];
-		}
-	 else {
+		} else {
 		console.log("Not enough resources.");
 	}
 }
@@ -488,9 +483,47 @@ document.getElementById("constructTower2ButtonMainCenterInfo").onclick=function(
 	tower2Constructor();
 }
 
-
-
+/*
+Kosten upgrades: 
+atk ship = 20 wood , 0 stone, 5 tools
+def ship = 20 wood , 0 stone, 5 tools
+def tower = 0 wood , 20 stone, 5 tools
+atk tower = 0 wood , 20 stone, 5 tools
+eco wood = 20 wood , 0 stone, 5 tools
+eco stone = 0 wood , 20 stone, 5 tools
+eco tools = 5 wood , 0 stone, 10 tools
+*/
 //TEST AREA
+
+
+let upgradeAtkShipLevel = 0;
+let upgradeAtkShipValue = [upgradeAtkShipLevel];
+
+function atkUpgradeShip(){
+	if (resources_array[0] > 20 && resources_array[2] > 5 ) {
+
+		let result_wood = Math.floor(resources_array[0] - 20);
+		let result_tools = Math.floor(resources_array[2] - 5);
+
+		resources_array[0] = result_wood;
+		resources_array[2] = result_tools;
+
+		upgradeAtkShipLevel += 10;
+
+
+			console.log(upgradeAtkShipLevel);
+			document.getElementById("atkUpgrade").innerHTML = Math.floor(upgradeAtkShipLevel / 10);
+			document.getElementById("resource_wood").innerHTML =resources_array[0];
+			document.getElementById("resource_iron").innerHTML = resources_array[2];
+		} else {
+			console.log("Not enough resources.");
+	}
+}
+
+document.getElementById("upgradeAtkShipButtonMainCenterInfo").onclick=function(){
+	atkUpgradeShip();
+}
+
 
 
 //COMBAT SYSTEM
@@ -507,36 +540,56 @@ let opponentTotalHealth = [2000];
 let opponentTotalAttack = [40];
 let currentLevel = 1;
 
-//weil properties multiplizieren zu schwer ist für JS. amount * atk/hp, for array summe = total atk/hp
+//amount * atk/hp, for array summe = total atk/hp
 function combatArray(){
 
 	let playerTotalHealth = 1;
 	
-	let militaryArrayTotalHealth = [militaryArrayPlayer[0] * 150, militaryArrayPlayer[1] * 225, militaryArrayPlayer[2] * 350, militaryArrayPlayer[3] * 325, militaryArrayPlayer[4] * 500]; 
+	let militaryArrayTotalHealth = [
+		militaryArrayPlayer[0] * 150, 
+		militaryArrayPlayer[1] * 225, 
+		militaryArrayPlayer[2] * 350, 
+		militaryArrayPlayer[3] * 325, 
+		militaryArrayPlayer[4] * 500]; 
 	
 	for(var i in militaryArrayTotalHealth) { playerTotalHealth += militaryArrayTotalHealth[i]; }
 	console.log("Total health player: " + playerTotalHealth);
 
 
-	let militaryArrayTotalAttack = [militaryArrayPlayer[0] * 25, militaryArrayPlayer[1] * 40, militaryArrayPlayer[2] * 65, militaryArrayPlayer[3] * 20, militaryArrayPlayer[4] * 40];
+	let militaryArrayTotalAttack = [
+		militaryArrayPlayer[0] * 25,
+		militaryArrayPlayer[1] * 40, 
+		militaryArrayPlayer[2] * 65, 
+		militaryArrayPlayer[3] * 20, 
+		militaryArrayPlayer[4] * 40];													
 
 	let playerTotalAttack = 1;
 
-	for(var i in militaryArrayTotalAttack) { playerTotalAttack += militaryArrayTotalAttack[i]; }
+
+
+	for(var i in militaryArrayTotalAttack) { Math.floor(playerTotalAttack += militaryArrayTotalAttack[i]); }
+	
+	/*Upgrades bonus % values*/
+	let upgradeAttackValue = parseInt(document.getElementById("atkUpgrade").innerHTML)*10;
+	let upgradeAtk = Math.floor(((upgradeAttackValue / 100) * playerTotalAttack));
+	
+	
 	console.log("Total attack player: " + playerTotalAttack);
+	console.log('Bonus ATK = ' + upgradeAtk);
 
 
 	
 	let combatTextArray = []; //nimmt mit .push daten aus while, gibt mit .join als string wieder
 
-	combatTextArray.push("Opponent total hp: " + opponentTotalHealth[0] + "\nPlayer total hp: " + playerTotalHealth);
+	combatTextArray.push(
+	"Opponent total hp: " + opponentTotalHealth[0] + "\nPlayer total hp: " + playerTotalHealth);
 
     while (playerTotalHealth && opponentTotalHealth[0] > 0) {
 
-		
         playerTotalHealth -= opponentTotalAttack[0];
-		opponentTotalHealth[0] -= playerTotalAttack;
-		console.log("Opponent.hp: " + opponentTotalHealth[0] + "\nPlayer.hp: " + playerTotalHealth);
+		opponentTotalHealth[0] -= (playerTotalAttack + upgradeAtk);
+
+		console.log ("Opponent.hp: " + opponentTotalHealth[0] + "\nPlayer.hp: " + playerTotalHealth);
 		combatTextArray.push("Opponent.hp: " + opponentTotalHealth[0] + "\nPlayer.hp: " + playerTotalHealth);
 
 		if (playerTotalHealth < 1) {
@@ -560,6 +613,7 @@ function combatArray(){
 			currentLevel +=1 ;
 			document.getElementById("menuCurrentLevelDisplay").innerHTML = ("Level: " + currentLevel);
 			alert("Victory!\n\nTo the next challenge!\n\n");
+
 			opponentTotalHealth = opponentTotalHealth.map(function(val){return opponentTotalHealth=3000;});
 			opponentTotalAttack = opponentTotalAttack.map(function(val){return opponentTotalAttack*2;});
 			}
@@ -568,6 +622,9 @@ function combatArray(){
 
 
 	
+
+
+
  //RESOURCE STATISTICS
 
 let resourceArrayStatisticMinute = [wood, stone, tools];
@@ -618,6 +675,8 @@ function resourceMenuCalcProductionMinute(){
 //MAP GENERATION
 
 
+
+
 /*function mapGenerationRandomizer() {
 	let mapTileArray = ["images/island_tile.png", "images/water_tile.gif"]
 	let num = Math.floor(Math.random() * 1);
@@ -652,5 +711,11 @@ function mapGenerationRandomizer() {
 	document.getElementById("menuReset").onclick=function() {
 	mapReset();
 }
+
+
+
+
+
+
 
 };
